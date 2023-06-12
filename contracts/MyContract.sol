13b7e24d5910c30e84998dc29991aeb0c41e72b3
tranbamanh229289 <manhtb@moonlab.asia>
2023-06-12 08:17:25 +0700

    update

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -2,9 +2,16 @@
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "contracts/MyToken.sol";
 
 contract MyContract is ERC20 {
     constructor() ERC20("MyToken", "MTK") {
         _mint(msg.sender, 100000000 * 10 ** decimals());
+    }
+
+    function buyNFT() external {
+        MyToken token = new MyToken();
+        transfer(msg.sender, 1000);
+        token.safeMint(msg.sender, uri);
     }
 }
